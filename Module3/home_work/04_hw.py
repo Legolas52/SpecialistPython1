67a6303bfa8de67be04a0f7c01f614f799fcd34c
Legolas52 <86655671+Legolas52@users.noreply.github.com>
2021-06-30 18:27:39 +0000

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,15 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+import random
+
+numbers = [-1, 10, 25, -9, 64, -81, 121, 81, 100, 76, -67, 225, -623, 625, 100, -91, 169]
+sqrt_numbers = []
+
+for numb in numbers:
+    if numb > 0:
+        if round(numb ** 0.5) == numb ** 0.5:
+            sqrt_numbers.append(int(numb ** 0.5))
+
+print(sqrt_numbers)
