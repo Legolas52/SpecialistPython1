ff77df0292f61baee54ddb639563841f840810c8
Legolas52 <86655671+Legolas52@users.noreply.github.com>
2021-07-01 11:40:20 +0000

    Update 08_task_func.py

--- Module4/practice/funcs_part1/08_task_func.py
+++ Module4/practice/funcs_part1/08_task_func.py
@@ -1,1 +1,12 @@
 # Напишите функцию находящую n-ое число Фибоначчи.
+def numb_fib(n):
+    if n == 1:
+        return 0
+    if n == 2 or n == 3:
+        return 1
+    return numb_fib(n - 1) + numb_fib(n - 2)
+
+
+print(numb_fib(10))
+print(numb_fib(3))
+print(numb_fib(5))
