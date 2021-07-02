c50af5f6f2fd31a9b6117637175404fcf4876fa9
Legolas52 <86655671+Legolas52@users.noreply.github.com>
2021-07-02 08:27:03 +0000

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,15 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
 
+def pagination2(num_items, items_on_page):
+    if num_items / items_on_page == int(num_items / items_on_page):
+        return items_on_page
+    return num_items % items_on_page
+
+
+print(pagination2(5, 2))
+print(pagination2(5, 1))
+print(pagination2(26, 7))
+print(pagination2(10, 2))
+print(pagination2(6, 3))
+print(pagination2(19, 3))
